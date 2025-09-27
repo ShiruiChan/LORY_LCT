37d4d2195c767f361c76127eef24bd3d2bca83e3
ShiruiSan <harlampevrenat@gmail.com>
2025-09-27 18:31:19 +0900

    fix/fixes vercel deply

--- src/app/layout/MobileShell.tsx
+++ src/app/layout/MobileShell.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { BottomNav } from "../components/BottomNav";
+import BottomNav from "../components/BottomNav";
 
 export default function MobileShell() {
   return (
